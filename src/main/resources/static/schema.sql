230de1c6ed4f83bb191c31bbb2d5bef7fe07b0db
mathias <mathias.egeberg@hotmail.no>
2024-04-04 14:42:45 +0200

    database & JS update

--- src/main/resources/static/schema.sql
+++ src/main/resources/static/schema.sql
@@ -1,1 +1,10 @@
 CREATE TABLE Bilett
+(
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    film VARCHAR(255) NOT NULL,
+    antall INT NOT NULL,
+    navn VARCHAR(255) NOT NULL,
+    telefon VARCHAR(255) NOT NULL,
+    epost VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
